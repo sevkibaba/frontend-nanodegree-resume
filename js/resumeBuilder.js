6030a05a2b2a8c94483901c88d2f3dd0f9b09fc0
sevkibaba <sevkiyontem@gmail.com>
2016-12-04 00:02:47 +0200

    A clean resume builder.js wit four objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,77 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs" : 
+	[
+		{
+			"employer": "JotForm",
+			"title": "Technical Support",
+			"location": "Hacettepe/Ankara"
+			"dates": "05/10/2016 - still working",
+			"description": "Helping JotForm customers at JotForm Suppot Forum",
+		}
+	]
+};
+
+var projects = {
+	"projects" : 
+	[
+		{
+			"title": "Maintenance Management System",
+			"dates": "2009 - 2011",
+			"description": "Sofware Requirement Analysis, Process Analysis, Software Testing, Software Education",
+			"images": ["http://gelisimpark.com.tr/en/euas-termik-santraller-isletim-ve-bakim-yonetimi/"]
+		}
+	]
+};
+
+var bio = {
+	'name' : name,
+	'role' : role,
+	'welcomeMessage' : 'May the force be with you',
+	'biopic' : 'images/vesika.jpg',	
+	'contacts' : {
+		'mobile' : '+90 543 819 65 88',
+		'email' : 'sevkiyontem@gmail.com',
+		'gitHub' : 'sevkibaba',
+		'twitter' : '@cropcoffeeshop',
+		'location' : 'Ankara/TURKEY',
+	},
+	'skills' : [
+		'programming', ' testing', ' process modelling', ' documentation',   
+	]
+};
+
+education = {
+	"schools" : 
+	[
+		{
+			"name" : "Middle East Technical University",
+			"location" : "Ankara",
+			"degree" : "BA",
+			"dates" : "2002 - 2008",
+			"url" : "http://ie.metu.edu.tr/",
+			"major" : ["Industrial Engineering"]
+		},
+		{
+			"name" : "Middle East Technical University",
+			"location" : "Ankara",
+			"degree" : "Master of Science",
+			"dates" : "2011 - 2015",
+			"url" : "http://ii.metu.edu.tr/general-information-about-cognitive-science-programs",
+			"major" : ["Cognitive Science"]
+
+		}
+
+	],
+	"onlinecourses" : 
+	[
+		{
+		"title" : "Javascript Crash Course",
+		"school" : "UDacity",
+		"dates" : "2017",
+		"url" : "https://classroom.udacity.com/courses/ud804/"
+		}
+	]
+};
+
+
+
